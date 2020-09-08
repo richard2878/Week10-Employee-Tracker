b81c63575b1edefeb8ff5808a75120801c38735a
Richard Antolin <richardantolin@gmail.com>
2020-09-07 17:47:28 -0700

    Revised Employee Database

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -27,42 +27,6 @@ CREATE TABLE department (
 
 CREATE TABLE allEmployeeDirectory SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, roles.title, roles.salary, department.name FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id;
 
--- Insert a set of records.
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jocelyn", "Chang", 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Andrew", "Lee", 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sean", "Lin", 2, NULL);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sabrina", "Lin", 3, 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Rebecca", "Lee", 4, NULL);
-
-INSERT INTO department (name)
-VALUES ("Sales");
-
-INSERT INTO department (name)
-VALUES ("Research & Development");
-
-INSERT INTO roles (title,salary,department_id)
-VALUES ("Sales rep",100000,1);
-
-INSERT INTO roles (title,salary,department_id)
-VALUES ("District Manager",500000,1);
-
-INSERT INTO roles (title,salary,department_id)
-VALUES ("Operation Specialist",80000,2);
-
-INSERT INTO roles (title,salary,department_id)
-VALUES ("Operation Manager",110000,2);
-
-
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
   ("Rodrigo", "Duterte", 1, NULL),
